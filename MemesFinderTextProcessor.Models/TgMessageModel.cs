35e15b7a415c5690e049134cf765fad4a26c25ab
KirillRukasov <shadowsun67@gmail.com>
2022-12-18 14:23:43 +0100

    add a model for tg messages with keyword

---
+++ MemesFinderTextProcessor.Models/TgMessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace MemesFinderTextProcessor.Models
+{
+    public class TgMessageModel
+    {
+        public Message Message { get; set; }
+        public string Keyword { get; set; }
+    }
+}
